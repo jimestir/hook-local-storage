095e3a431b1231d66e937ff6046a5eda2c623af2
jimmyvergara03 <jimmyestir@gmail.com>
2022-04-14 18:37:41 -0400

    Add localStorage for text save

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState(JSON.parse(localStorage.getItem("V1")));
+
+  function setLocalStorge(event) {
+    const value = event.target.value;
+    try {
+      setText(value);
+      localStorage.setItem("V1", JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <div className="App">
         <textarea
           className="App__textarea"
           placeholder="Ya has hecho tu custom hook?"
-          onChange={(event) => console.log(event.target.value)}
+          value={text}
+          onChange={setLocalStorge}
         ></textarea>
       </div>
     </>
